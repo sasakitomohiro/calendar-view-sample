aada9a8e9af44d71894cc1c1af17cf6076f85e55
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-11-24 12:14:24 +0900

    delete unused import

--- app/src/main/java/monster/sasakisan/calendar_view_sample/MainActivity.kt
+++ app/src/main/java/monster/sasakisan/calendar_view_sample/MainActivity.kt
@@ -1,12 +1,12 @@
 package monster.sasakisan.calendar_view_sample
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
-import com.kizitonwose.calendarview.model.*
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
 import monster.sasakisan.calendar_view_sample.databinding.ActivityMainBinding
 import org.threeten.bp.YearMonth
