ec00e6fa893106e97d4c4ec5611fca5fc0554bb8
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-11-24 10:45:09 +0900

    add binding

--- app/src/main/java/monster/sasakisan/calendar_view_sample/MainActivity.kt
+++ app/src/main/java/monster/sasakisan/calendar_view_sample/MainActivity.kt
@@ -2,11 +2,17 @@ package monster.sasakisan.calendar_view_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import monster.sasakisan.calendar_view_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.calendarView
     }
 }
